f44aa4c461f761b10639aad485bf7ca4729a9e5b
Niza <ebenezerundie@gmail.com>
2024-06-10 10:21:07 +0100

    cloud functions

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,73 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_functions import firestore_fn
+from firebase_admin import firestore, initialize_app, credentials
+from google.cloud.firestore_v1.base_query import FieldFilter
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+cred = credentials.ApplicationDefault()
+app = initialize_app(cred)
+db = firestore.client()
+
+
+#Triggering the function to calculate rating for the product each time an existing review is updated
+#or deleted or a new review is created
+
+@firestore_fn.on_document_written(document="reviews/{review_id}")
+def calculate_product_rating(event: firestore_fn.Event[firestore_fn.Change[firestore_fn.DocumentSnapshot | None]]) -> None:
+
+
+    #initialized to zero 
+    before_rating = 0
+    after_rating = 0
+
+    #keeps track of events (if it is an update, deletion or creation)
+    review_count = 0
+
+    
+
+    if event.data.before is not None:
+        before_rating = event.data.before.get('rating')
+        try:
+            product_id = event.data.before.get('product_id')
+            review_count -= 1
+        except KeyError:
+            return
+
+
+    if event.data.after is not None:
+        after_rating = event.data.after.get('rating')
+        try:
+            product_id = event.data.after.get('product_id')
+            review_count += 1
+        except KeyError:
+            return
+
+
+    #Get the product with the product id
+    product_list = db.collection('products').where(filter=FieldFilter('id','==',product_id)).get()
+
+    if(len(product_list)>0):
+        product = product_list[0]
+        try: 
+            product_data = product.to_dict()
+            review_info = product_data.get('review_info', {})
+            total_rating = review_info.get('total_rating', 0)
+            total_reviews = review_info.get('total_reviews',0)
+            
+        except KeyError: 
+            total_rating = 0
+            total_reviews = 0
+        
+        total_rating = total_rating + after_rating - before_rating
+        total_reviews = total_reviews + review_count
+
+        if total_reviews == 0:
+            average_rating = 0
+        else:
+            average_rating = round(total_rating / total_reviews,1)
+
+        db.collection('products').document(product.id).update({'review_info': {"average_rating": average_rating, "total_reviews": total_reviews, 'total_rating': total_rating}})
+
+
+
+
+
